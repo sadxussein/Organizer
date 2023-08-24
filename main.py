4788d7c54d779429bef75e7853d9b9493af987a4
sadxussein <kvdworking@gmail.com>
2023-08-25 00:22:46 +0300

    modified gui

--- main.py
+++ main.py
@@ -27,151 +27,151 @@ if __name__ == "__main__":
     window.show()
     app.exec()
 
-    # 1. Parse JSON file into simple list of Task elements
-    with open("tasks.json", 'r') as file:   # JSON file containing our tasks
-        dictToParse = json.load(file)       # store all our tasks which now need to be parsed into simple list
-    taskList = []                           # this one
-    for taskName, taskDesc in dictToParse.items():
-        task = Task(taskName, *taskDesc)       # using second constructor
-        taskList.append(task)                  # adding to list
-
-    # 2. Endless loop for user to enter commands via match-case
-    while True:
-        print("What do you want to do? Type '?' for help.")
-        command = input()   # initial input from user
-        match command:
-            # 3. Help menu option
-            case '?':   # help menu
-                print("? - help\n"
-                      "n - new task\n"
-                      "l - list tasks\n"
-                      "r - remove task\n"
-                      "e - edit task\n"
-                      "s - start task\n"
-                      "f - finish task\n"
-                      "q - quit program\n")
-            # 4. Create new task option
-            # !!! add description field
-            case 'n':   # add new task
-                print("Type your task:")
-                text = input()
-                if len(text) > 0:   # task name length should be > 0
-                    if not any(task.name == text for task in taskList):     # check if task already exists in taskDict
-                        task = Task(text)       # creating new instance of Task class
-                        taskList.append(task)   # adding to list
-                    else:
-                        print("Task with this name is already present in registered tasks.")
-                else:
-                    print("Task name cannot be empty.")
-            # 5. List all tasks verbose description
-            case 'l':   # show all currently available tasks
-                if len(taskList) > 0:   # if there are any tasks
-                    print("Current tasks available:")
-                    for task in taskList:
-                        for key, value in task.get_task("USER").items():
-                            print(f"{key}: {value}")  # info about the task: name, when task was registered etc
-                else:
-                    print("There are no tasks registered.")
-            # 6. Remove single task
-            # !!! rework so user could edit/remove/add multiple tasks
-            case 'r':   # remove task
-                if len(taskList) > 0:   # if there are any tasks
-                    print("Select task number you want to remove:")
-                    # print formatted list with indexes
-                    print('\n'.join(f"{index+1}: {task.get_task('NAME')}" for index, task in enumerate(taskList)))
-                    taskNumber = input()
-                    try:    # check for correct input from user
-                        taskNumber = int(taskNumber)
-                    except ValueError:  # if input is incorrect
-                        print("Please input an integer.")
-                        continue    # if not valid integer we skip this iteration of the loop
-                    if taskNumber - 1 in range(len(taskList)):      # if selected task is present in taskList
-                        print("Task", taskList[taskNumber - 1].name, "removed.")
-                        del taskList[taskNumber - 1]      # remove it
-                    else:
-                        print("Please input a valid task index.")
-                else:
-                    print("No tasks registered.")
-            # 7. Edit single task
-            case 'e':   # edit task
-                if len(taskList) > 0:  # if there are any tasks
-                    print("Select task number you want to edit:")
-                    print('\n'.join(f"{index + 1}: {task.get_task('NAME')}" for index, task in enumerate(taskList)))
-                    taskNumber = input()
-                    try:  # check for correct input from user
-                        taskNumber = int(taskNumber)
-                    except ValueError:  # if input is incorrect
-                        print("Please input an integer.")
-                        continue  # if not valid integer we skip this iteration of the loop
-                    if taskNumber - 1 in range(len(taskList)):  # if selected task is present in taskList
-                        print("Please enter new name for selected task:")
-                        text = input()
-                        if len(text) > 0:  # task name length should be > 0
-                            if not any(task.name == text for task in taskList):  # if task exists in taskDict
-                                taskList[taskNumber - 1].set_task(text)
-                            else:
-                                print("Task with this name is already present in registered tasks.")
-                        else:
-                            print("Task name cannot be empty.")
-                    else:
-                        print("Please input a valid task index.")
-                else:
-                    print("No tasks registered.")
-            # 8. Start tracking task
-            case 's':
-                # If there are any tasks and at least one of them is not running
-                if len(taskList) > 0 and not all(task.get_task("RUNNING") for task in taskList):
-                    print("Select task number you want to start:")
-                    print('\n'.join(f"{index + 1}: {task.get_task('NAME')}" for index, task in enumerate(taskList)))
-                    taskNumber = input()
-                    try:  # check for correct input from user
-                        taskNumber = int(taskNumber)
-                    except ValueError:  # if input is incorrect
-                        print("Please input an integer.")
-                        continue  # if not valid integer we skip this iteration of the loop
-                    if taskNumber - 1 in range(len(taskList)):  # if selected task is present in taskList
-                        if taskList[taskNumber - 1].get_task("RUNNING"):
-                            print("Task", taskList[taskNumber - 1].get_task("NAME"), "is already running.")
-                        else:
-                            print("Task", taskList[taskNumber - 1].get_task("NAME"), "started.")
-                            taskList[taskNumber - 1].start_task()  # start task, wait for finish
-                    else:
-                        print("Please input a valid task index.")
-                elif all(task.get_task("RUNNING") for task in taskList):
-                    print("All tasks running.")
-                elif len(taskList) == 0:
-                    print("No tasks registered.")
-            # 9. Finish tracking task
-            case 'f':
-                if len(taskList) > 0 and any(task.get_task("RUNNING") for task in taskList):  # if there are any tasks
-                    print("Select task number you want to finish:")
-                    print('\n'.join(f"{index + 1}: {task.get_task('NAME')}" for index, task in enumerate(taskList)))
-                    taskNumber = input()
-                    try:  # check for correct input from user
-                        taskNumber = int(taskNumber)
-                    except ValueError:  # if input is incorrect
-                        print("Please input an integer.")
-                        continue  # if not valid integer we skip this iteration of the loop
-                    if taskNumber - 1 in range(len(taskList)):  # if selected task is present in taskList
-                        if not taskList[taskNumber - 1].get_task("RUNNING"):
-                            print("Task", taskList[taskNumber - 1].get_task("NAME"), "is already stopped.")
-                        else:
-                            print("Task", taskList[taskNumber - 1].get_task("NAME"), "stopped.")
-                            taskList[taskNumber - 1].end_task()  # start task, wait for finish
-                    else:
-                        print("Please input a valid task index.")
-                elif not any(task.get_task("RUNNING") for task in taskList):
-                    print("No tasks running.")
-                elif len(taskList) == 0:
-                    print("No tasks registered.")
-            # 10. Exit and save tasks
-            case 'q':   # finishing program and printing result to JSON file
-                with open("tasks.json", 'w') as file:   # open file
-                    dumpDict = {}                       # the result dict
-                    for task in taskList:               # pick every task we have
-                        dumpDict.update(task.get_task("JSON"))  # placing them all in the result dict
-                    json.dump(dumpDict, file, indent=4)         # finally, saving file, ending program
-                print("Goodbye.")
-                break
-            case _:
-                print("Unrecognized command. Please try again.")
+    # # 1. Parse JSON file into simple list of Task elements
+    # with open("tasks.json", 'r') as file:   # JSON file containing our tasks
+    #     dictToParse = json.load(file)       # store all our tasks which now need to be parsed into simple list
+    # taskList = []                           # this one
+    # for taskName, taskDesc in dictToParse.items():
+    #     task = Task(taskName, *taskDesc)       # using second constructor
+    #     taskList.append(task)                  # adding to list
+    #
+    # # 2. Endless loop for user to enter commands via match-case
+    # while True:
+    #     print("What do you want to do? Type '?' for help.")
+    #     command = input()   # initial input from user
+    #     match command:
+    #         # 3. Help menu option
+    #         case '?':   # help menu
+    #             print("? - help\n"
+    #                   "n - new task\n"
+    #                   "l - list tasks\n"
+    #                   "r - remove task\n"
+    #                   "e - edit task\n"
+    #                   "s - start task\n"
+    #                   "f - finish task\n"
+    #                   "q - quit program\n")
+    #         # 4. Create new task option
+    #         # !!! add description field
+    #         case 'n':   # add new task
+    #             print("Type your task:")
+    #             text = input()
+    #             if len(text) > 0:   # task name length should be > 0
+    #                 if not any(task.name == text for task in taskList):     # check if task already exists in taskDict
+    #                     task = Task(text)       # creating new instance of Task class
+    #                     taskList.append(task)   # adding to list
+    #                 else:
+    #                     print("Task with this name is already present in registered tasks.")
+    #             else:
+    #                 print("Task name cannot be empty.")
+    #         # 5. List all tasks verbose description
+    #         case 'l':   # show all currently available tasks
+    #             if len(taskList) > 0:   # if there are any tasks
+    #                 print("Current tasks available:")
+    #                 for task in taskList:
+    #                     for key, value in task.get_task("USER").items():
+    #                         print(f"{key}: {value}")  # info about the task: name, when task was registered etc
+    #             else:
+    #                 print("There are no tasks registered.")
+    #         # 6. Remove single task
+    #         # !!! rework so user could edit/remove/add multiple tasks
+    #         case 'r':   # remove task
+    #             if len(taskList) > 0:   # if there are any tasks
+    #                 print("Select task number you want to remove:")
+    #                 # print formatted list with indexes
+    #                 print('\n'.join(f"{index+1}: {task.get_task('NAME')}" for index, task in enumerate(taskList)))
+    #                 taskNumber = input()
+    #                 try:    # check for correct input from user
+    #                     taskNumber = int(taskNumber)
+    #                 except ValueError:  # if input is incorrect
+    #                     print("Please input an integer.")
+    #                     continue    # if not valid integer we skip this iteration of the loop
+    #                 if taskNumber - 1 in range(len(taskList)):      # if selected task is present in taskList
+    #                     print("Task", taskList[taskNumber - 1].name, "removed.")
+    #                     del taskList[taskNumber - 1]      # remove it
+    #                 else:
+    #                     print("Please input a valid task index.")
+    #             else:
+    #                 print("No tasks registered.")
+    #         # 7. Edit single task
+    #         case 'e':   # edit task
+    #             if len(taskList) > 0:  # if there are any tasks
+    #                 print("Select task number you want to edit:")
+    #                 print('\n'.join(f"{index + 1}: {task.get_task('NAME')}" for index, task in enumerate(taskList)))
+    #                 taskNumber = input()
+    #                 try:  # check for correct input from user
+    #                     taskNumber = int(taskNumber)
+    #                 except ValueError:  # if input is incorrect
+    #                     print("Please input an integer.")
+    #                     continue  # if not valid integer we skip this iteration of the loop
+    #                 if taskNumber - 1 in range(len(taskList)):  # if selected task is present in taskList
+    #                     print("Please enter new name for selected task:")
+    #                     text = input()
+    #                     if len(text) > 0:  # task name length should be > 0
+    #                         if not any(task.name == text for task in taskList):  # if task exists in taskDict
+    #                             taskList[taskNumber - 1].set_task(text)
+    #                         else:
+    #                             print("Task with this name is already present in registered tasks.")
+    #                     else:
+    #                         print("Task name cannot be empty.")
+    #                 else:
+    #                     print("Please input a valid task index.")
+    #             else:
+    #                 print("No tasks registered.")
+    #         # 8. Start tracking task
+    #         case 's':
+    #             # If there are any tasks and at least one of them is not running
+    #             if len(taskList) > 0 and not all(task.get_task("RUNNING") for task in taskList):
+    #                 print("Select task number you want to start:")
+    #                 print('\n'.join(f"{index + 1}: {task.get_task('NAME')}" for index, task in enumerate(taskList)))
+    #                 taskNumber = input()
+    #                 try:  # check for correct input from user
+    #                     taskNumber = int(taskNumber)
+    #                 except ValueError:  # if input is incorrect
+    #                     print("Please input an integer.")
+    #                     continue  # if not valid integer we skip this iteration of the loop
+    #                 if taskNumber - 1 in range(len(taskList)):  # if selected task is present in taskList
+    #                     if taskList[taskNumber - 1].get_task("RUNNING"):
+    #                         print("Task", taskList[taskNumber - 1].get_task("NAME"), "is already running.")
+    #                     else:
+    #                         print("Task", taskList[taskNumber - 1].get_task("NAME"), "started.")
+    #                         taskList[taskNumber - 1].start_task()  # start task, wait for finish
+    #                 else:
+    #                     print("Please input a valid task index.")
+    #             elif all(task.get_task("RUNNING") for task in taskList):
+    #                 print("All tasks running.")
+    #             elif len(taskList) == 0:
+    #                 print("No tasks registered.")
+    #         # 9. Finish tracking task
+    #         case 'f':
+    #             if len(taskList) > 0 and any(task.get_task("RUNNING") for task in taskList):  # if there are any tasks
+    #                 print("Select task number you want to finish:")
+    #                 print('\n'.join(f"{index + 1}: {task.get_task('NAME')}" for index, task in enumerate(taskList)))
+    #                 taskNumber = input()
+    #                 try:  # check for correct input from user
+    #                     taskNumber = int(taskNumber)
+    #                 except ValueError:  # if input is incorrect
+    #                     print("Please input an integer.")
+    #                     continue  # if not valid integer we skip this iteration of the loop
+    #                 if taskNumber - 1 in range(len(taskList)):  # if selected task is present in taskList
+    #                     if not taskList[taskNumber - 1].get_task("RUNNING"):
+    #                         print("Task", taskList[taskNumber - 1].get_task("NAME"), "is already stopped.")
+    #                     else:
+    #                         print("Task", taskList[taskNumber - 1].get_task("NAME"), "stopped.")
+    #                         taskList[taskNumber - 1].end_task()  # start task, wait for finish
+    #                 else:
+    #                     print("Please input a valid task index.")
+    #             elif not any(task.get_task("RUNNING") for task in taskList):
+    #                 print("No tasks running.")
+    #             elif len(taskList) == 0:
+    #                 print("No tasks registered.")
+    #         # 10. Exit and save tasks
+    #         case 'q':   # finishing program and printing result to JSON file
+    #             with open("tasks.json", 'w') as file:   # open file
+    #                 dumpDict = {}                       # the result dict
+    #                 for task in taskList:               # pick every task we have
+    #                     dumpDict.update(task.get_task("JSON"))  # placing them all in the result dict
+    #                 json.dump(dumpDict, file, indent=4)         # finally, saving file, ending program
+    #             print("Goodbye.")
+    #             break
+    #         case _:
+    #             print("Unrecognized command. Please try again.")
--- main_window.py
+++ main_window.py
@@ -1,36 +1,90 @@
 from PyQt5.uic import loadUi
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QWidget
+from task_table_model import TaskTableModel
 import json
+
+
+# TODO: create new task form
+# TODO: connect them with calendar
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        # init JSON handling structure
+        self.json_model = QStandardItemModel()
+        self.table_model = TaskTableModel()
         # load UI file
         loadUi("main.ui", self)
         self.init_window()
 
     def init_window(self):
         # create JSON model for input file
+        self.open_json_file()
+        # use tree element created from UI
+        # self.tree_view.setModel(self.json_model)
+        self.table_view.setModel(self.table_model)
+        self.save_file.clicked.connect(lambda: self.save_json_file(self.json_model))
+        self.add_task.clicked.connect(lambda: self.on_add_task(self.json_model))
+        self.delete_task.clicked.connect(lambda: self.on_delete_task(self.json_model))
+        self.modify_task.clicked.connect(lambda: self.on_modify_task(self.json_model))
+
+    # opening file and extracting data
+    def open_json_file(self):
         with open("tasks.json", 'r') as file:   # JSON file containing our tasks
             json_data = json.load(file)         # store all our tasks which now need to be parsed into simple list
-        json_model = QStandardItemModel()
         # import from file into model
-        self.import_json_file(json_model, json_data)
-        # use tree element created from UI
-        self.treeView.setModel(json_model)
+        self.json_to_model(self.json_model, json_data)
 
+    # transforming JSON to QStandardItemModel
     # recursively adding data from nodes to tree view
-    def import_json_file(self, parent_item, data):
+    def json_to_model(self, parent_item, data):
         if isinstance(data, dict):
             for key, value in data.items():
                 key_item = QStandardItem(str(key))
                 parent_item.appendRow(key_item)
-                self.import_json_file(key_item, value)
+                self.json_to_model(key_item, value)
         elif isinstance(data, list):
             for value in data:
-                self.import_json_file(parent_item, value)
+                self.json_to_model(parent_item, value)
         else:
             value_item = QStandardItem(str(data))
             parent_item.appendRow(value_item)
+
+    # saving to file results of program's work
+    def save_json_file(self, model):
+        root = model.invisibleRootItem()
+        json_data = self.model_to_json(root)
+        with open("test.json", "w") as file:
+            json.dump(json_data, file, indent=4)
+
+    # transforming model back to JSON to save in file
+    def model_to_json(self, parent_item):
+        if parent_item.hasChildren():
+            result = {}
+            test_result = []
+            for row in range(parent_item.rowCount()):
+                print(parent_item.text(), parent_item.child(row).text())
+                child_item = parent_item.child(row)
+                test_result.append(child_item.text())
+                result[child_item.text()] = self.model_to_json(child_item)
+            print(test_result)
+            return result
+        else:
+            return parent_item.text()
+
+    # add task button function
+    def on_add_task(self, parent_item):
+        print("added task")
+        pass
+
+    # delete task button function
+    def on_delete_task(self, parent_item):
+        print("deleted task")
+        pass
+
+    # modify task button function
+    def on_modify_task(self, parent_item):
+        print("modified task")
+        pass
